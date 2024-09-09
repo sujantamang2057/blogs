5557fd11ce95a5ae9c0c063b0e399581aaa50f4d
sujan <sujandontamang@gmail.com>
2024-09-09 15:42:47 +0545

    category model active and inactive count shown in the dashboard and also change in the controller logic for the blog model active nad inactive count for better efficienct

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Models\blog_category;
 
 class DashboardController extends Controller
 {
@@ -10,10 +11,25 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $activecount = Blog::where('status', '1')->count();
-        $inactivecount = Blog::where('status', '0')->count();
+        //blog count
+        $blogCounts = Blog::whereIn('status', [0, 1])
+            ->select('status', Blog::raw('count(*) as count'))
+            ->groupBy('status')
+            ->pluck('count', 'status');
 
-        return view('admin.dashboard.index', compact('activecount', 'inactivecount'));
+        $activecount = $blogCounts[1] ?? 0; // active blogs count
+        $inactivecount = $blogCounts[0] ?? 0; // inactive blogs count
+
+        //category count
+        $categoryCounts = blog_category::whereIn('status', [0, 1])
+            ->select('status', blog_category::raw('count(*) as count'))
+            ->groupBy('status')
+            ->pluck('count', 'status');
+
+        $activecount1 = $categoryCounts[1] ?? 0; //  category active blogs count
+        $inactivecount1 = $categoryCounts[0] ?? 0; //  categoryinactive blogs count
+
+        return view('admin.dashboard.index', compact('activecount', 'inactivecount', 'activecount1', 'inactivecount1'));
     }
 
     public function newblogs()
