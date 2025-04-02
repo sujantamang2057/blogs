e1317ac8703f86d37ffe8c4d982865553dca8a7f
Sujan.Tamang <sujan_mi@yonefu.info>
2025-04-02 18:24:56 +0545

    6.20

--- resources/views/admin/album/create.blade.php
+++ resources/views/admin/album/create.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-sm-6">
 
-                    <h1>test album merge here  6.17</h1>
+                    <h1>test album merge here  6.20</h1>
                     <p class="text-muted">Create a new album</p>
                 </div>
                 <div class="col-sm-6">
